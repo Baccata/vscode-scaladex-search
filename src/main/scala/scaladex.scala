943a9ee95f81f46ff1432d1fa6ebd004ba92a030
Carlos de Paula <me@carlosedp.com>
2023-08-25 11:38:48 -0300

    Query scaladex for both Scala 2.13 and 3 joining results

--- src/main/scala/scaladex.scala
+++ src/main/scala/scaladex.scala
@@ -6,16 +6,27 @@ object scaladex {
   import ops._
 
   val target = "JVM"
-  val scalaVersion = "2.13"
+  val scalaVersion = Seq("2.13", "3")
 
   def search(name: String) = {
     val n = name.trim()
-    fetch(
-      s"https://index.scala-lang.org/api/search?q=$n&target=$target&scalaVersion=$scalaVersion"
-    ).flatMap(_.json).map { json =>
-      json.asInstanceOf[js.Array[Project]].filter { project =>
-        project.repository.contains(n) || project.artifacts
-          .exists(_.contains(n))
+
+    val searchResults = scalaVersion.map { version =>
+      fetch(
+        s"https://index.scala-lang.org/api/search?q=$n&target=$target&scalaVersion=$version"
+      ).flatMap(_.json).map { json =>
+        json.asInstanceOf[js.Array[Project]].filter { project =>
+          project.repository.contains(n) || project.artifacts
+            .exists(_.contains(n))
+        }
+      }
+    }
+
+    searchResults.reduce { (a, b) =>
+      a.flatMap { a =>
+        b.map { b =>
+          (a ++ b).distinct
+        }
       }
     }
   }
